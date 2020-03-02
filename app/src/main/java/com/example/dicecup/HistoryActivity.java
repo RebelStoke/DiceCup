fe42f54de11ceebf79e3a168206675b947f0ab61
jakubrewald <jakubrewald@gmail.com>
2020-03-02 22:26:02 +0100

    Release

--- app/src/main/java/com/example/dicecup/HistoryActivity.java
+++ app/src/main/java/com/example/dicecup/HistoryActivity.java
@@ -26,11 +26,32 @@ public class HistoryActivity extends AppCompatActivity {
 
     public void populateHistory(){
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_list_item_1, rollHistoryList);
+                this, android.R.layout.simple_list_item_1, prepareValues(rollHistoryList));
         lv.setAdapter(arrayAdapter);
     }
 
     public void closeHistory(View view) {
         finish();
+    }
+
+    public void clearHistory(View view){
+        MainActivity.rollHistory.clear();
+        rollHistoryList.clear();
+        populateHistory();
+    };
+
+    public ArrayList<String> prepareValues(ArrayList<String> list){
+        ArrayList<String> newList = new ArrayList<>();
+        for (String string: list) {
+            String formatedString = "";
+            for(int i = 0; i < string.length(); i++){
+                formatedString += string.charAt(i);
+                if(i != string.length() - 1){
+                    formatedString += "-";
+                }
+            }
+            newList.add(formatedString);
+        }
+        return newList;
     }
 }
--- app/src/main/java/com/example/dicecup/MainActivity.java
+++ app/src/main/java/com/example/dicecup/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.dicecup;
 import  androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
@@ -16,32 +18,45 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     public List<Dice> dices = new ArrayList<>();
     public List<TableRow> rows = new ArrayList<>();
-    public ArrayList<String> rollHistory = new ArrayList<>();
+    public static ArrayList<String> rollHistory = new ArrayList<>();
     TableLayout tl;
+    int orientNum = 2;
+    int dicesInView = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tl = findViewById(R.id.diceContainer);
+        addDice(null);
     }
 
-    public void addDice(View view) {
+    public Dice addDice(View view) {
+        if(dices.size() > 5)
+            return null;
         Dice dice = new Dice(this);
         dices.add(dice);
-        setUpDices(dice);
+        setUpDices(dice, orientNum);
+        return dice;
     }
     public void deleteDice(View view) {
+        if(dices.size() == 1)
+            return;
+        dicesInView--;
         Dice dice = dices.get(dices.size() - 1);
         dices.remove(dice);
         for (TableRow row : rows) {
             row.removeView(dice);
+            if(row.getVirtualChildCount() == 0)
+                rows.remove(row);
         }
     }
 
-    public void setUpDices(Dice dice){
-        if(dices.size() % 3 == 0){
+    public void setUpDices(Dice dice, int number){
+        dicesInView++;
+        if((dicesInView - 1)  % number == 0){
             TableRow tr2 = new TableRow(this);
+            tr2.setGravity(Gravity.CENTER);
             tr2.addView(dice);
             tl.addView(tr2);
             rows.add(tr2);
@@ -62,12 +77,34 @@ public class MainActivity extends AppCompatActivity {
     }
     public int getRandomNumber(){
         Random random = new Random();
-        return random.nextInt(5) + 1;
+        return random.nextInt(6) + 1;
     }
 
     public void openHistory(View view) {
         Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
         intent.putStringArrayListExtra("RollHistory", rollHistory);
         startActivity(intent);
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        dicesInView = 0;
+        super.onConfigurationChanged(newConfig);
+        tl.removeAllViews();
+        rows.clear();
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            orientNum = 3;
+            for (Dice dice: dices) {
+               TableRow t = (TableRow)dice.getParent();
+               t.removeView(dice);
+                setUpDices(dice, 3);
+            }
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            orientNum = 2;
+            for (Dice dice: dices) {
+                TableRow t = (TableRow)dice.getParent();
+                t.removeView(dice);
+                setUpDices(dice, 2);
+            }
+        }
     }
 }
